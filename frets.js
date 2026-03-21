d4ddacc5d50f5c991a0091991d6c31cb495eab2e
Shayne Rushton <mshaynerush@gmail.com>
2026-03-21 16:55:34 -0500

    Update frets.js

--- frets.js
+++ frets.js
@@ -33,7 +33,7 @@ function fretsTest() {
         } else {
             fretWidthReduction =  2 + (bitDiameter)
         }
-        currentFretWidth = (((fret/totalLength) * base1) + ((totalLength - fret)/totalLength) * base2)) - fretWidthReduction
+        currentFretWidth = ((fret/totalLength) * base1) + ((totalLength - fret)/totalLength) * base2)) - fretWidthReduction
         currentFretWidth = Number(currentFretWidth).toFixed(3)
         fretboardWidths.push(currentFretWidth)
     })
