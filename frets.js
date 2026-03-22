178c88ea8f8e2344a01a921996922fe213b63e79
Shayne Rushton <mshaynerush@gmail.com>
2026-03-21 19:09:47 -0500

    Update frets.js

--- frets.js
+++ frets.js
@@ -49,7 +49,9 @@ function fretsTest() {
         fretboardWidths.push(currentFretWidth)
     })
  
-
+        fretBoardWidths.map(FB => (
+            console.log(FB)
+            )
         createGCode(yDistances, fretboardWidths)
 }
 
