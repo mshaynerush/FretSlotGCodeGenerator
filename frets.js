6a762d40d05e808ffbb37c0268a3a23d6ab63cae
Shayne Rushton <mshaynerush@gmail.com>
2026-03-21 19:50:07 -0500

    Update frets.js

--- frets.js
+++ frets.js
@@ -39,18 +39,12 @@ function fretsTest() {
 
         console.log("fretWidthReduction: " + fretWidthReduction)
         console.log("fret: " + fret)
-        currentFretWidth = ((fret/totalLength) * base1) 
-        console.log("currentFretWidth: " + currentFretWidth)
-        currentFretWidth += ((totalLength - (fret/totalLength)) * base2) 
-        console.log("currentFretWidth: " + currentFretWidth)
-        currentFretWidth -= fretWidthReduction
-        console.log("currentFretWidth: " + currentFretWidth)
+        currentFretWidth = ((fret/totalLength) * base1) + ((totalLength - (fret/totalLength)) * base2) - fretWidthReduction
         console.log("fret Distance: " + fret)
         console.log("currentFretWidth: " + currentFretWidth)
         fretboardWidths.push(currentFretWidth)
     })
  
-        fretboardWidths.map(FB => (
         createGCode(yDistances, fretboardWidths)
 }
 
