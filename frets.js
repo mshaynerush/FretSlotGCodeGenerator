d3487df19bb4e2df8f7df18722b8c8fd33d6c348
Shayne Rushton <mshaynerush@gmail.com>
2026-03-21 21:04:06 -0500

    Update frets.js

--- frets.js
+++ frets.js
@@ -43,9 +43,9 @@ function fretsTest() {
         byBase2 = totalLength - byBase1
         console.log("Base 1: " + byBase1 + " base2: " + byBase2)
 
-        // currentFretWidth = ((fret/totalLength) * base1) 
-        // currentFretWidth += ((totalLength - (fret/totalLength)) * base2) 
-        // currentFretWidth -= fretWidthReduction
+        currentFretWidth = ((fret/totalLength) * base1) 
+        currentFretWidth += ((totalLength - (fret/totalLength)) * base2) 
+        currentFretWidth -= fretWidthReduction
     
         console.log("currentFretWidth: " + currentFretWidth)
         fretboardWidths.push(currentFretWidth)
